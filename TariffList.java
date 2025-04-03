f3f6ba11b73cdca6f4318a76b7122b1fd8284678
Augustin Redon <augustin.redon@icloud.com>
2025-04-03 10:44:34 -0400

    added different classes

---
+++ TariffList.java
@@ -0,0 +1,11 @@
+public class TariffList implements TariffPolicy{
+
+    @Override
+    public String evaluateTrade(double proposedTariff, double minimumTariff) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'evaluateTrade'");
+    }
+
+    //inner class TariffNode
+    
+}
--- TradeManager.java
+++ TradeManager.java
@@ -1,6 +1,6 @@
 
 
-public class Main {
+public class TradeManager {
 
     public static void main(String[] args){
         System.out.println("Hello World");
