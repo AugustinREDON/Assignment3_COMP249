2837ada83eed43850a12890fef9dc72b67d83997
Jacob Paterak <jacobpaterak@gmail.com>
2025-04-05 20:55:05 -0400

    Almost finished part 1

--- Product.java
+++ Product.java
@@ -1,5 +1,34 @@
-public class Product {
+public class Product implements Comparable<Product> {
+    private String productName;
+    private String Country;
+    private String Category;
+    private double price;
 
+    public Product(String productName, String Country, String Category, double price) {
+        this.productName = productName;
+        this.Country = Country;
+        this.Category = Category;
+        this.price = price;
+    }
+    public String getProductName() {
+        return this.productName;
+    }
+    public String getCountry() {
+        return this.Country;
+    }
+    public String getCategory() {
+        return this.Category;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+    public int compareTo(Product other) {
+        return this.productName.compareToIgnoreCase(other.productName);
+    }
+    public String toString() {
+        return "Product{name='" + productName + "', country='" + Country +
+                "', category='" + Category + "', price=" + price + "}";
+    }
     
     
 }
--- TradeManager.java
+++ TradeManager.java
@@ -1,10 +1,93 @@
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+public class TradeManager{
 
-public class TradeManager {
+    public static void main(String[] args) throws FileNotFoundException , IOException{
 
-    public static void main(String[] args){
-        System.out.println("Hello World");
+        BufferedReader br = new BufferedReader(new FileReader("TradeData.txt"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("UpdatedTradeData.txt"));
+        ArrayList<Product> Products =  new ArrayList<Product>();
+
+        try{
+            Scanner sc = new Scanner(br);
+            String filepath = "TradeData.txt";
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                try{
+                    Scanner linescan = new Scanner(line);
+                    linescan.useDelimiter(",");
+
+
+                    //Have to add this to an arraylist
+                    String ProductName = linescan.next();
+                    String country = linescan.next();
+                    String category = linescan.next();
+                    double InitialPrice = linescan.nextDouble();
+                    switch (country) {
+                        case "China":
+                            InitialPrice *= 1.25;
+                            break;
+                        case "USA":
+                            if (category.equals("Electronics"))
+                                InitialPrice *= 1.1;
+                            break;
+                        case "Japan":
+                            if (category.equals("Automobiles"))
+                                InitialPrice *= 1.15;
+                            break;
+                        case "India":
+                            if (category.equals("Agriculture"))
+                                InitialPrice *= 1.05;
+                            break;
+                        case "South Korea":
+                            if (category.equals("Electronics"))
+                                InitialPrice *= 1.08;
+                            break;
+                        case "Saudi Arabia":
+                            if (category.equals("Energy"))
+                                InitialPrice *= 1.12;
+                            break;
+                        case "Germany":
+                            if (category.equals("Manufacturing"))
+                                InitialPrice *= 1.06;
+                            break;
+                        case "Bangladesh":
+                            if (category.equals("Textile"))
+                                InitialPrice *= 1.04;
+                            break;
+                        case "Brazil":
+                            if (category.equals("Agriculture"))
+                                InitialPrice *= 1.09;
+                            break;
+                    }
+                    Products.add(new Product(ProductName, country, category, InitialPrice));
+
+
+
+                    //We want to check which country
+                    //Check which category it is
+                    //Add the right tariff and add it to the updated file
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+
+            }
+        }
+        catch(Exception e){
+
+        }
+        Products.sort(null);
+        System.out.println(Products.toString());
+        bw.close();
+        br.close();
+
+
+
     }
+
 
 
 }
